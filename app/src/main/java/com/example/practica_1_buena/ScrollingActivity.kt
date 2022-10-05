5e404ed90faa344b9e5750f79ee820871174eb87
Ivan <ivanhg1999@gmail.com>
2022-10-05 18:21:02 +0200

    esta en ello

---
+++ app/src/main/java/com/example/practica_1_buena/ScrollingActivity.kt
@@ -0,0 +1,37 @@
+package com.example.practica_1_buena
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.example.practica_1_buena.databinding.ActivityScrollingBinding
+
+class ScrollingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadImage()
+        }
+
+    private fun loadImage(url: String =
+                              "https://img.vavel.com/fb32yeawyaaypa8-1-1662376801173.jpg")
+    {
+        Glide.with(this)
+            .load(url)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .centerCrop()
+            .into(binding.content.imgCard2)
+    }
+
+    }
